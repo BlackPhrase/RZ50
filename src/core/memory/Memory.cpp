04f306374d405b7d65f0ddfc2d597de08336c58a
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-03-31 09:35:06 +0000

    Move core's memory module code to 'memory' subfolder

--- src/core/memory/Memory.cpp
+++ src/core/memory/Memory.cpp
@@ -1,23 +1,23 @@
 /* 
  * This file is part of RZ Engine
- * Copyright (c) 2017-2018 BlackPhrase
+ * Copyright (c) 2017-2019 BlackPhrase
  * 
  * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ * it under the terms of the GNU Lesser General Public License as published by  
  * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU Lesser General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "Memory.hpp"
 
-namespace rz
+namespace rz::core::memory
 {
 
 // TODO: Apply usage of mempools here
@@ -56,4 +56,4 @@ void CMemory::FreePool(IMemPool &apPool)
 		};
 };
 
-}; // namespace rz
+}; // namespace rz::core::memory
