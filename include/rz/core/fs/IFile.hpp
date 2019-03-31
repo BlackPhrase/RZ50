1a14b4558a4629b8023ba1f53d06127e83de4845
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-03-31 09:56:28 +0000

    Move core's fs module interfaces to 'fs' subfolder

--- include/rz/core/fs/IFile.hpp
+++ include/rz/core/fs/IFile.hpp
@@ -1,23 +1,23 @@
 /* 
  * This file is part of RZ Engine
- * Copyright (c) 2017-2018 BlackPhrase
+ * Copyright (c) 2017-2019 BlackPhrase
  * 
  * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ * it under the terms of the GNU Lesser General Public License as published by  
  * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU Lesser General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
 
-namespace rz
+namespace rz::core
 {
 
 struct IFile
@@ -35,4 +35,4 @@ struct IFile
 	virtual const char *GetPath() const = 0;
 };
 
-}; // namespace rz
+}; // namespace rz::core
