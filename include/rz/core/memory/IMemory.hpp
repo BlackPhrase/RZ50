031730732b595dc9379741a78419a38cc6ab25d4
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-03-31 09:51:17 +0000

    Move core's memory module interfaces to 'memory' subfolder

--- include/rz/core/memory/IMemory.hpp
+++ include/rz/core/memory/IMemory.hpp
@@ -1,31 +1,32 @@
 /* 
  * This file is part of RZ Engine
- * Copyright (c) 2017-2018 BlackPhrase
+ * Copyright (c) 2017-2019 BlackPhrase
  * 
  * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ * it under the terms of the GNU Lesser General Public License as published by  
  * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU Lesser General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
 
-#include <cstring>
-#include "CoreTypes.hpp"
+#include <cstddef>
+
+#include "core/CoreTypes.hpp"
 
 // Internal core allocator shared between the modules
 // By using it we can improve portability of the dlls
 // because we're using the memory from the single crt of the core
 // module
 
-namespace rz
+namespace rz::core::memory
 {
 
 struct IMemPool;
@@ -45,22 +46,4 @@ struct IMemory
 	virtual void FreePool(IMemPool &apPool) = 0;
 };
 
-struct IMemPool
-{
-	/// Alloc specified memory inside the pool
-	virtual void *Alloc(size_t anSize) = 0;
-	
-	/// Free the mem from the pool
-	virtual void Free(void *apData) = 0;
-	
-	///
-	virtual void Clear() = 0;
-	
-	///
-	virtual size_t GetUsedSize() const = 0;
-	
-	///
-	virtual size_t GetTotalSize() const = 0;
-};
-
-}; // namespace rz
+}; // namespace rz::core::memory
