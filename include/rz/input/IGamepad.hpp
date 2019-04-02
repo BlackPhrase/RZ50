2763d277f9a2e229042d2c8fa47cc58d1b320a9d
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-04-02 13:06:37 +0000

    Initial gamepad input device interface

---
+++ include/rz/input/IGamepad.hpp
@@ -0,0 +1,35 @@
+/* 
+ * This file is part of RZ Engine
+ * Copyright (c) 2019 BlackPhrase
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "IInputDevice.hpp"
+
+namespace rz::input
+{
+
+struct IGamepad : public IInputDevice
+{
+	static IInputDevice::Type Type{IInputDevice::Type::Gamepad};
+	
+	virtual bool IsKeyDown(Key aeKey) const = 0;
+	virtual bool IsAnyKeyDown() const = 0;
+	
+	const char *GetDeviceName() const override final {return "Gamepad";}
+};
+
+}; // namespace rz::input
