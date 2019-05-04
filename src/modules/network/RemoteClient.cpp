303425f5853f2a94c0b3cf2b4fbfcbf0d176918b
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-05-04 10:18:09 +0000

    Update network module implementation (null impl -> default impl) (2)

--- src/modules/network/RemoteClient.cpp
+++ src/modules/network/RemoteClient.cpp
@@ -1,23 +1,23 @@
 /* 
  * This file is part of RZ Engine
- * Copyright (c) 2017-2018 BlackPhrase
+ * Copyright (c) 2017-2019 BlackPhrase
  * 
  * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ * it under the terms of the GNU Lesser General Public License as published by  
  * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU Lesser General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "RemoteClient.hpp"
 
-namespace rz
+namespace rz::network
 {
 
 bool CRemoteClient::Reconnect()
@@ -29,4 +29,4 @@ void CRemoteClient::Disconnect()
 {
 };
 
-}; // namespace rz
+}; // namespace rz::network
